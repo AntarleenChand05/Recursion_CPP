be63eba3163b326d11908b50b5e26b2df0383f71
AntarleenChand05 <antarleen.chand.btech2024@sitpune.edu.in>
2025-09-24 01:36:50 -0400

    Create Sum of n Numbers.cpp

---
+++ Sum of n Numbers.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int sum(int n)
+{
+    if (n<=1)
+        return n;
+    else 
+        return n + sum(n-1);
+}
+int main()
+{
+    int num;
+    cout<<"Enter a number: "<<endl;
+    cin>>num;
+    cout<<"Sum is: "<<sum(num);
+}
+// Output
+// Enter a number:
+// 10
+// Sum is: 55
